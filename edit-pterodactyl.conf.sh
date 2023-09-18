fd207b1d1a66e3c721b89d0f7cca9b8a7ee56ad9
nwcfollowthewhiterabbit <100003631+nwcfollowthewhiterabbit@users.noreply.github.com>
2023-09-18 12:18:15 +0300

    Update edit-pterodactyl.conf.sh

---
+++ edit-pterodactyl.conf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Попросить пользователя ввести новое значение для server_dns_name
+read -p "Введите новое значение для server_dns_name: " server_dns_name
+
+# Проверить, было ли введено значение для server_dns_name
+if [ -z "$server_dns_name" ]; then
+  echo "Значение для server_dns_name не было введено. Выход из скрипта."
+  exit 1
+fi
+
+# Файл конфигурации nginx (Pterodactyl.conf), который нужно отредактировать
+NGINX_CONF_FILE="/etc/nginx/sites-available/Pterodactyl.conf"
+
+# Выполнить замену значения в файле конфигурации nginx
+sed -i "s/server_name <domain>;$/server_name $server_dns_name;/" "$NGINX_CONF_FILE"
+
+# Проверить, была ли замена успешной
+if [ $? -eq 0 ]; then
+  echo "Замена значения в файле $NGINX_CONF_FILE выполнена успешно."
+else
+  echo "Ошибка: Замена значения в файле $NGINX_CONF_FILE не удалась."
+fi
